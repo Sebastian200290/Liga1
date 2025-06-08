f58a04dfcba5cf61b914dec91021f854083f8da8
Sebastian200290 <156244428+Sebastian200290@users.noreply.github.com>
2025-06-08 19:47:24 +0200

    Poprawiona konfiguracja bazy MySQL

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ from models import db, Team, Player, Game, Wynik, Gol
 import os
 
 app = Flask(__name__)
-# Ścieżka do bazy w Azure App Service:
-db_user = os.getenv('DB_USER')
-db_pass = os.getenv('DB_PASS')
-db_host = os.getenv('DB_HOST')
-db_name = os.getenv('DB_NAME')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}?charset=utf8mb4"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'SQLITE_URL',
+    'sqlite:///liga.db'
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.before_first_request
-def init_db():
+# 🔧 UTWÓRZ BAZĘ – to nowy sposób, zamiast @before_first_request
+with app.app_context():
     db.create_all()
 
 @app.route('/')
